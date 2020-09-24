4b9c41373d7f7a30a2415f9d0b7a9adbc694275a
Peter <Peter@163.com>
2020-09-24 10:10:43 +0800

    解决冲突

--- src/main/java/com/javakc/Test1.java
+++ src/main/java/com/javakc/Test1.java
@@ -7,5 +7,9 @@ public class Test1 {
         System.out.println("777");
         System.out.println("javakc1 hi");
         System.out.println("javakc1 hello");
+        System.out.println("666");
+        System.out.println("777");
+        System.out.println("javakc1 hi");
+        System.out.println("javakc1 hello");
     }
 }
