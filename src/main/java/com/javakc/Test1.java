608bbf5e3d0c976eb7e4aa52635f7f85aefd8f2d
Peter <Peter@163.com>
2020-09-24 09:40:43 +0800

    制造冲突

--- src/main/java/com/javakc/Test1.java
+++ src/main/java/com/javakc/Test1.java
@@ -4,6 +4,6 @@ public class Test1 {
 
     public static void main(String[] args) {
         System.out.println("qq");
-        System.out.println("javakc1");
+        System.out.println("javakc1 hello");
     }
 }
