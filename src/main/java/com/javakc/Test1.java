6cf892f9beeee67cbdfcfa25afb90eb3b782e63b
Peter <Peter@163.com>
2020-09-24 09:54:45 +0800

    666

--- src/main/java/com/javakc/Test1.java
+++ src/main/java/com/javakc/Test1.java
@@ -3,7 +3,7 @@ package com.javakc;
 public class Test1 {
 
     public static void main(String[] args) {
-        System.out.println("qq");
+        System.out.println("666");
         System.out.println("javakc1 hi");
         System.out.println("javakc1 hello");
     }
