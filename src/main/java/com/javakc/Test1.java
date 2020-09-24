b54e4c8514dc423d09aa8f9bb73ce3a738cd4b40
Peter <Peter@163.com>
2020-09-24 10:05:10 +0800

    解决冲突

--- src/main/java/com/javakc/Test1.java
+++ src/main/java/com/javakc/Test1.java
@@ -4,6 +4,7 @@ public class Test1 {
 
     public static void main(String[] args) {
         System.out.println("666");
+        System.out.println("777");
         System.out.println("javakc1 hi");
         System.out.println("javakc1 hello");
     }
